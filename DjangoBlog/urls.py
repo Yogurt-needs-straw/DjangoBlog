07105c1cfc4dee4be147ad08877235bd416155f2
养乐多要吸管 <a102467@outlook.com>
2024-04-22 20:14:51 +0800

    create models

--- DjangoBlog/urls.py
+++ DjangoBlog/urls.py
@@ -16,6 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from api import views
+
 urlpatterns = [
     # path('admin/', admin.site.urls),
+    # path('db/', views.db), # 创建一些数据
+
+    path('api/blog/', views.BlogView.as_view()),
+
 ]
--- api/views.py
+++ api/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+from api import models
+
+
+def db(request):
+
+    v1 = models.UserInfo.objects.create(username="admin", password="123")
+    v2 = models.UserInfo.objects.create(username="x1", password="123")
+
+    models.Blog.objects.create(
+        category=1,
+        image="xxx/xxx.png",
+        title="x经理",
+        summary=".....",
+        text="xxxxxxxxxx",
+        creator=v1
+    )
+
+    models.Blog.objects.create(
+        category=2,
+        image="xxx/xxx.png",
+        title="a经理",
+        summary=".....cccccc",
+        text="xxxxxxxxxxdddddddddd",
+        creator=v2
+    )
+
+    return HttpResponse("成功")
+
+
+class BlogView:
+    pass
